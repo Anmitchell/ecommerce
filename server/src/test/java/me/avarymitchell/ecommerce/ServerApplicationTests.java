b401d903b1db579c0c86494c28298559c834b491
Avary Mitchell <avarymitchell@Avarys-MacBook-Pro.local>
2026-02-21 07:01:58 -0500

    chore: enforce cross-platform line endings via .gitattributes

---
+++ server/src/test/java/me/avarymitchell/ecommerce/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.avarymitchell.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
